f6929fa37177fcef56d83d935b744beef6f09849
NightlyD3V <TylerSpaulding95@gmail.com>
2019-04-22 00:24:41 +0100

    Added three.js files, working cube, fixing lifecycle bug

--- src/components/TheGame.js
+++ src/components/TheGame.js
@@ -1,6 +1,8 @@
+import * as THREE from 'three';
+import ReactDOM from 'react-dom';
+import { Canvas } from 'react-three-fiber';
 import React from 'react';
 import styled from 'styled-components';
-import * as THREE from 'three';
 
 const MyCanvas = styled.canvas`
     background-color: black;
@@ -8,28 +10,34 @@ const MyCanvas = styled.canvas`
     height: 100%;
 `;
 
-const TheGame = () => {
-    return (
-        <div>
-            <MyCanvas width='100%'></MyCanvas>
-            <script>
-            //newScene
-                let scene = new THREE.Scene();
-                let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/ window.innerHeight, 0.1, 1000 );
-                let renderer = new THREE.WebGLRenderer();
-                renderer.setSize( window.innerWidth, window.innerHeight );
-                document.body.appendChile( renderer.domElement );
-            //geometry 
-                let geometry = new THREE.BoxGeometry( 1, 1, 1 );
-            //material
-                let material = new THREE.MeshBasicMaterial({ color: '0xff000' });
-            //cube 
-                let cube = new THREE.Mesh( geometry, material );
-            //camera position 
-            camera.position.z = 5; 
-            </script>
-        </div>
-    )
+class TheGame extends React.Component {
+
+    render() {
+        let scene = new THREE.Scene();
+        let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        let renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        var cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        camera.position.z = 5;
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        function animate() {
+            requestAnimationFrame( animate );
+            renderer.render( scene, camera );
+        }
+        animate();
+        return (
+            <div>
+            
+            </div>
+        )
+    }
 }
 
 export default TheGame
